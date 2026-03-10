d11083638eae7d99a527395b9128a8183da3b60d
Asaduzzaman Rahim <rahim.asaduzzaman@gmail.com>
2026-03-10 09:41:08 +0600

    Icon Setup Completed

--- src/Components/Benar.jsx
+++ src/Components/Benar.jsx
@@ -15,7 +15,7 @@ const Benar = () => {
             <Flex className="items-end flex-wrap-reverse">
                 <div className='py-[60px]  lg:pt-[100px] lg:pb-[120px] '>
                     <h1 className='text-[45px] md:text-[40px] lg:text-[63px] font-semibold font-Outfit leading-15 lg:leading-[80px] text-white'>Book Appointment <br /> With Trusted Doctors</h1>
-                    <div className='flex flex-wrap items-center gap-3.5 pt-[10px] pb-[30px]'>
+                    <div className='flex flex-wrap lg:flex-nowrap items-center gap-3.5 pt-[10px] pb-[30px]'>
                         <img className='max-w-[130px] max-h-[56px]' src={GroupPhoto} alt="Group Photo" />
                         <p className=' text-[14px] md:text-[18px] font-Outfit leading-[27px] text-white'>Simply browse through our extensive list of trusted doctors, schedule your appointment hassle-free.</p>
                     </div>
